1cee93e7d673f1f8bb34296f494a3c416e377c14
TmTutui <tulio.tutui@gmail.com>
2023-04-03 19:15:54 +0200

    partie 2 done

--- FenPrincipale.py
+++ FenPrincipale.py
@@ -9,7 +9,7 @@ class FenPrincipale(tk.Tk):
 
 
         self.title("Clavier virtuel")
-        self.geometry("500x550")  
+        self.geometry("500x580")  
 
         self.configure(bg="#c0d9e2")
 
@@ -22,28 +22,45 @@ class FenPrincipale(tk.Tk):
         self.bouton_nouvelle_partie = tk.Button(self, text="Nouvelle partie", command=self.nouvelle_partie)
         self.bouton_nouvelle_partie.pack( side = tk.TOP, pady=10)
         
+        self.mot_a_deviner = tk.StringVar()
+        self.motActuel = tk.StringVar()
+        self.mot_a_deviner.set("")
+        self.motActuel.set("START A NEW GAME")
+        self.label_mot = tk.Label(self, textvariable=self.motActuel, font=("Courier", 16))
+        self.label_mot.pack(pady=10)
+        self.coupsRestants = 10
+        self.lettresJouees = []        
+        
+        def cliquer(self):
+            self.config(state=tk.DISABLED)
+            self.traitement(self.lettre)
+        
         self.boutons = []
+
         for i in range(26):
             lettre = chr(ord('A')+i)
-            bouton = tk.Button(self.zone_affichage,  text=lettre, state=tk.DISABLED)
+            bouton= tk.Button( self.zone_affichage, text=lettre,  command = lambda index =i: self.cliquer(index), state=tk.DISABLED)
+            #bouton = tk.Button(self.zone_affichage,  text=lettre, command= self.cliquer(), state=tk.DISABLED)
             bouton.grid(row=i//7, column=i%7, padx=5, pady=5, sticky="nsew")  # Use a Grid layout manager
             self.boutons.append(bouton)
 
-
-
-
+        # self.bouton= tk.Button( self, text=lettre,  command = self.cliquer, state=tk.DISABLED)
+        # self.bouton.pack( side = tk.TOP, pady=10)
+       
         self.zone_dessin = tk.Canvas(self, width=200, height=200, bg="white",insertborderwidth=10)
         self.zone_dessin.pack(pady=10)
 
+        self.etatPartie = tk.StringVar()
+        self.etatPartie.set("Vous avez " + str(self.coupsRestants) + ' coups restants')
+        self.label_etatPartie= tk.Label(self, textvariable=self.etatPartie, font=("Courier", 16))
+        self.label_etatPartie.pack(pady=10)
+
         self.__mots = self.charger_mots()
 
-        self.mot_a_deviner = tk.StringVar()
-        self.mot_a_deviner.set("")
-        self.label_mot = tk.Label(self, textvariable=self.mot_a_deviner, font=("Courier", 16))
-        self.label_mot.pack(pady=10)
-
-
-
+    def cliquer(self, index):
+        self.boutons[index].config(state=tk.DISABLED)
+        lettre = self.boutons[index]['text']
+        self.traitement(lettre)
 
     def charger_mots(self):
         mots = []
@@ -53,12 +70,75 @@ class FenPrincipale(tk.Tk):
         return mots
 
     def nouvelle_partie(self):
+        self.coupsRestants = 10
+        self.etatPartie.set("Vous avez " + str(self.coupsRestants) + ' coups restants')
+
         mot = random.choice(self.__mots)
         self.mot_a_deviner.set(mot)
+        self.motActuel.set(len(mot)*'*')
+        self.label_mot = tk.Label(self, textvariable=self.motActuel, font=("Courier", 16))
+
         for bouton in self.boutons:
-            bouton.config(state=tk.NORMAL)
+             bouton.config(state=tk.NORMAL)
         self.zone_dessin.delete("all")
 
     def quitter(self):
         self.destroy()
+
+    def traitement(self, lettre):
+
+        if self.mot_a_deviner is None:
+            return
+        
+        mot = self.mot_a_deviner.get()
+        self.lettresJouees.append(lettre)
+        
+        if lettre not in (mot):
+            self.coupsRestants -= 1
+            self.etatPartie.set("Vous avez " + str(self.coupsRestants) + ' coups restants')
+            #print('perdi uma vida')
+            #self.afficherPendu()
+            
+        for i in range(len(mot)):
+            if str(mot)[i] == lettre:
+                copy_to_str = list(str(self.motActuel.get()))
+                copy_to_str[i]=lettre
+                lettreReveal = "".join(copy_to_str)
+                self.motActuel.set(lettreReveal)
+        
+        # self.afficherMot()
+        print(str(self.motActuel.get()))
+        print(str(mot))
+        if str(self.motActuel.get()) == str(mot):
+            for bouton in self.boutons:
+                bouton.config(state=tk.DISABLED)
+            print("Vous avez gagné !")
+            self.etatPartie.set("Vous avez gagné !")
+        elif self.coupsRestants <= 0:
+            for bouton in self.boutons:
+                bouton.config(state=tk.DISABLED)
+            self.etatPartie.set("Vous avez perdu !")
+            print("Vous avez perdu !")
+
+# class Button(FenPrincipale):
+#         def __init__(self):
+#             self.boutons = []
+#             for i in range(26):
+#                 lettre = chr(ord('A')+i)
+#                 bouton= tk.Button( self.zone_affichage, text=lettre,  state=tk.DISABLED)
+#                 #bouton = tk.Button(self.zone_affichage,  text=lettre, command= self.cliquer(), state=tk.DISABLED)
+#                 bouton.grid(row=i//7, column=i%7, padx=5, pady=5, sticky="nsew")  # Use a Grid layout manager
+#                 self.button.append(bouton)
+                
+# class MonBoutonLettre(tk.Button):
+    
+#     def __init__(self,  lettre):
+#         self = tk.Button(self.zone_affichage,  text=lettre, command= self.cliquer(), state=tk.DISABLED)
+#         self.lettre = lettre
+        
+    
+#     def cliquer(self):
+#         self.config(state=tk.DISABLED)
+#         FenPrincipale.traitement(self.lettre)
+#         print('oiii cliquei')
 
--- ZoneAffichage.py
+++ ZoneAffichage.py
@@ -4,11 +4,11 @@ class ZoneAffichage(tk.Frame):
     def __init__(self, master):
         super().__init__(master, bg="#c0d9e2")
 
-        self.boutons = []  # Initialize list of buttons
+        # self.boutons = []  # Initialize list of buttons
 
-        # Create buttons for each letter of the alphabet
-        for i in range(26):
-            letter = chr(ord('A') + i)  # Convert index to corresponding letter
-            button = tk.Button(self, text=letter)
-            button.grid(row=i//7, column=i%7, padx=1, pady=1)  # Position button in a 6x5 grid
-            self.boutons.append(button)  # Add button to list
+        # # Create buttons for each letter of the alphabet
+        # for i in range(26):
+        #     letter = chr(ord('A') + i)  # Convert index to corresponding letter
+        #     button = tk.Button(self, text=letter)
+        #     button.grid(row=i//7, column=i%7, padx=1, pady=1)  # Position button in a 6x5 grid
+        #     self.boutons.append(button)  # Add button to list
